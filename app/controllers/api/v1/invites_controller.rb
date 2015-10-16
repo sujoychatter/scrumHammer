ab5b492e9a808939b72b76bff407dd48358d1ae9
Ritesh Kumar <ritesh.kumar@housing.com>
2015-10-16 22:12:08 +0530

    minor changes

--- app/controllers/api/v1/invites_controller.rb
+++ app/controllers/api/v1/invites_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::InvitesController < ApplicationController
 	def send_invite
-		render :json => {:user_id => 1}
+		user = current_user
+		if user
+			# send email to user
+			render :json => current_user
+		else
+			render :json => {status: 403}
+		end
 	end
 end
