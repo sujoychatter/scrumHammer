7d343637a30faa3572ba470a0bad818e3c6562d5
Ritesh Kumar <ritesh.kumar@housing.com>
2015-10-16 16:18:56 +0530

    team controller created

---
+++ app/controllers/api/v1/teams_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::TeamsController < ApplicationController
+	def show 
+		render :json => {:user_id => 1}
+	end
+	def update 
+		render :json => {:user_id => 1}
+	end
+	def create 
+		render :json => {:user_id => 1}
+	end
+	def destroy 
+		render :json => {:user_id => 1}
+	end
+end
